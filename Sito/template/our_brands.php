a6f0c6d1f0c8846ca42d83a1a4441780fa598fe8
Claudia Falcone <claudia.falcone3@studio.unibo.it>
2025-02-04 11:00:56 +0100

    Added img and other brands

--- Sito/template/our_brands.php
+++ Sito/template/our_brands.php
@@ -2,61 +2,89 @@
 <div class="col-md-10 mt-5">
     <h3 class="text-center">I nostri Brand</h3>
     <div class="row text-center mb-3">
-        <div class="col-md-3">
-            <div class="card ">           
-                <div class="card-body">                
+        <div class="col-md-2">
+            <div class="card ">
+                <div class="card-body">
                 <a href="marchi.php?brand=Briantos"><img class="card-img-top img-fluid w-50 mx-auto" src="./imgs/logo_Briantos.jpg" alt="Briantos" /></a>
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
-            <div class="card">            
-                <div class="card-body"> 
-                <a href="marchi.php?brand=Natural Trainer"><img class="card-img-top img-fluid  w-50 mx-auto" src="./imgs/logo_NaturalTrainer.jpg" alt="Natural Trainer" /></a>                 
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card">            
+        <div class="col-md-2">
+            <div class="card">
                 <div class="card-body">
-                <a href="marchi.php?brand=TIAKI"><img class="card-img-top img-fluid  w-50 mx-auto" src="./imgs/Tiaki_Logo.jpg" alt="TIAKI" /></a>                    
+                <a href="marchi.php?brand=Natural Trainer"><img class="card-img-top img-fluid  w-50 mx-auto" src="./imgs/logo_NaturalTrainer.jpg" alt="Natural Trainer" /></a>
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
-            <div class="card">            
+        <div class="col-md-2">
+            <div class="card">
+                <div class="card-body">
+                <a href="marchi.php?brand=TIAKI"><img class="card-img-top img-fluid  w-50 mx-auto" src="./imgs/Tiaki_Logo.jpg" alt="TIAKI" /></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="card">
                 <div class="card-body">
                 <a href="marchi.php?brand=KONG"><img class="card-img-top img-fluid  w-50 mx-auto" src="./imgs/logo_Kong.jpg" alt="KONG" /></a>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row text-center mb-5">
-        <div class="col-md-3">
-            <div class="card">            
+        <div class="col-md-2">
+            <div class="card">
                 <div class="card-body">
-                <a href="marchi.php?brand=Taste Of The Wild"><img class="card-img-top img-fluid" src="./imgs/logo_TasteOfTheWild.jpg" alt="Taste Of The Wild" /></a>
+                <a href="marchi.php?brand=Taste Of The Wild"><img class="card-img-top img-fluid w-50 mx-auto" src="./imgs/logo_TasteOfTheWild.jpg" alt="Taste Of The Wild" /></a>
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
-            <div class="card">            
+        <div class="col-md-2">
+            <div class="card">
                 <div class="card-body">
                 <a href="marchi.php?brand=Almo Nature"><img class="card-img-top img-fluid  w-50 mx-auto" src="./imgs/logo_AlmoNature.jpg" alt="Almo Nature" /></a>
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
-            <div class="card">            
+    </div>
+    <div class="row text-center mb-3">
+         <div class="col-md-2">
+            <div class="card">
                 <div class="card-body">
                 <a href="marchi.php?brand=Trixie"><img class="card-img-top img-fluid  w-50 mx-auto" src="./imgs/trixie_3.png" alt="Trixie" /></a>
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
-            <div class="card">                       
+        <div class="col-md-2">
+            <div class="card">
                 <div class="card-body">
                 <a href="marchi.php?brand=Royal Canin"><img class="card-img-top img-fluid  w-50 mx-auto" src="./imgs/logo_RoyalCanin.jpg" alt="Royal Canin" /></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="card">
+                <div class="card-body">
+                <a href="marchi.php?brand=Barkoo"><img class="card-img-top img-fluid  w-50 mx-auto" src="./imgs/logo_Barkoo.jpg" alt="Barkoo" /></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="card">
+                <div class="card-body">
+                <a href="marchi.php?brand=Phil & Sons"><img class="card-img-top img-fluid  w-50 mx-auto" src="./imgs/logo_Phil_&_Sons.jpg" alt="Phil & Sons" /></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="card">
+                <div class="card-body">
+                <a href="marchi.php?brand=Nomad"><img class="card-img-top img-fluid  w-50 mx-auto" src="./imgs/logo_Nomad.jpg" alt="Nomad" /></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="card">
+                <div class="card-body">
+                <a href="marchi.php?brand=Wolf of Wilderness"><img class="card-img-top img-fluid  w-50 mx-auto" src="./imgs/logo_Wolf_of_Wilderness.jpg" alt="Wolf of Wilderness" /></a>
                 </div>
             </div>
         </div>
